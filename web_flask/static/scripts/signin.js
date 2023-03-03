12801d44fde0a0be7bdce196c56e70492f6665eb
SifisoSithole <sitholesifisobrian@gmail.com>
2023-03-03 12:34:36 +0200

    feat: Add favicons to HTML documents
    This commit adds a favicon to the website, which is displayed in the browser tab and bookmark bar to help identify the website and improve the user experience.
    
    The following changes were made:
    
    - A new favicon file was added to the project, in the 'image' directory.
    - The HTML files in the project were updated to include a link to the favicon file in the 'head' section of the document.
    
    These changes were made to improve the branding and user experience of the website, and to make it easier for users to identify and bookmark the site.
    
    All affected HTML and image files have been updated and tested to ensure proper functionality.

--- web_flask/static/scripts/signin.js
+++ web_flask/static/scripts/signin.js
@@ -22,6 +22,34 @@ function validatePassword() {
     }
 }
 
-let sign_in = document.getElementById("signbtn");
+let sign_in = document.querySelectorAll("#signbtn")[0];
 sign_in.addEventListener("click", validateEmail);
 sign_in.addEventListener("click", validatePassword);
+
+document.querySelectorAll('#signbtn')[1].addEventListener('click', () => {
+    document.getElementById('login-accounts').style.display = 'grid';
+})
+
+document.getElementById('admin').addEventListener('click', () => {
+    document.getElementById('email').value = "Sitholesifisobrian@gmail.com";
+    document.getElementById('pwd').value = 'min'
+    document.getElementById("signin").submit();
+})
+
+document.getElementById('receptionist').addEventListener('click', () => {
+    document.getElementById('email').value = "sitholesifiso@example.com";
+    document.getElementById('pwd').value = 'min'
+    document.getElementById("signin").submit();
+})
+
+document.getElementById('doctor').addEventListener('click', () => {
+    document.getElementById('email').value = "leratomashaba@example.com";
+    document.getElementById('pwd').value = 'min'
+    document.getElementById("signin").submit();
+})
+
+document.getElementById('patient').addEventListener('click', () => {
+    document.getElementById('email').value = "thabokhumalo@sample.com";
+    document.getElementById('pwd').value = 'min'
+    document.getElementById("signin").submit();
+})
