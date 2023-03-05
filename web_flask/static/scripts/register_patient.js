ab8410855180d8059a4f3991446e9641194b2502
SifisoSithole <sitholesifisobrian@gmail.com>
2023-03-05 11:47:04 +0200

    feat: Implement receptionist account type
    This commit includes the implementation of the receptionist account type, which allows users with this account type to register new patients, make appointments for patients, and check in patients. The receptionist account type was added to improve the overall functionality and usability of the system.
    
    The following changes were made:
    
    - A new receptionist account type was added, with the ability to register new patients, make appointments for patients, and check in patients.
    - The user interface was updated to include the receptionist account type in the list of available account types.
    - The server-side code was updated to include the necessary functionality for the receptionist account type.
    
    These changes were made to improve the overall functionality and usability of the system, and to provide users with more options for managing patient data and appointments.
    
    All affected code has been updated and tested to ensure proper functionality.

---
+++ web_flask/static/scripts/register_patient.js
@@ -0,0 +1,48 @@
+const form = document.getElementById("register_patient");
+// Listen for the form's submit event
+form.addEventListener('submit', function(event) {
+
+    // validate form fields
+    let add_user = popupWindow.document.getElementById("add");
+    add_user.addEventListener("click", validateName);
+    add_user.addEventListener("click", validateLastName);
+    add_user.addEventListener("click", validateEmail);
+    add_user.addEventListener("click", validatePassword);
+
+    // Stop the form from submitting normally
+    event.preventDefault();
+
+    // Create a new object to hold the form data
+    const formData = {};
+
+    // Use jQuery's serializeArray() method to extract the form data into an array of objects
+    const dataArray = $(this).serializeArray();
+
+    // Loop over the array of form data objects and add them to the formData object
+    $.each(dataArray, function() {
+        formData[this.name] = this.value;
+    });
+
+    popupWindow.close();
+    // Use jQuery's $.ajax() method to send the data to the server
+    $.ajax({
+        url: 'https://www.healtheasehub.live/signup',
+        method: 'POST',
+        data: formData,
+        success: function(response) {
+        // Handle a successful response from the server
+        if (response.result === 'exists') {
+            alert("Email address already registered")
+        } else if (response.result === 'failed') {
+            alert("Failed to create User, please try again")
+        } else {
+            window.location.reload();
+            alert("User created")
+        }
+        },
+        error: function(xhr, status, error) {
+        // Handle an error response from the server
+        alert("Failed to create User, please try again")
+        }
+    });
+});
